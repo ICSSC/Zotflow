cad0d38584fe284500db87ffe54a3a472f01d4e5
Brian Bui <bbui195@gmail.com>
2019-03-06 17:36:01 -0800

    Addded json file for next ICS classes and code to get next classes from transcript classes

--- scripts/transcript_parser.py
+++ scripts/transcript_parser.py
@@ -1,10 +1,10 @@
 
 
 
-
+from collections import defaultdict
 
 FILE_NAME = 'transcript_example.txt'
-
+NEXT_CLASSES = 'ICS_next.json'
 
 
 def run_parser() -> None:
@@ -23,10 +23,44 @@ def run_parser() -> None:
             class_dict['number'] = line_text[2]
             classes.append(class_dict)
     print(classes)
-        
+    return classes
 
     transcript.close()
 
+def get_next_classes(classes):
+    global next_classes
+    
+    possible = set()
+    current_classes = set()
+    for c in classes:
+        if c['department'] == 'I&C SCI':
+            current_classes.add(c['number'])
+            if c['number'] in next_classes:
+                possible = possible.union(set(next_classes[c['number']]))
+
+    prereqs = defaultdict(set)
+    
+    for k, v in next_classes.items():
+        same = possible.intersection(set(v))
+        for c in same:
+            prereqs[c].add(k)
+    can_take = set()
+    for k, v in prereqs.items():
+        done = v.difference(current_classes)
+        if len(done) == 0:
+            can_take.add(k)
+            
+    print('\n', can_take)
+    return can_take
+    
+    
+        
 
 if __name__ == '__main__':
-    run_parser()
+    classes = run_parser()
+    global next_classes
+    classes_file = open(NEXT_CLASSES, 'r')
+    next_classes = eval(''.join(classes_file.readlines()))
+    classes_file.close()
+    #print('\n', next_classes)
+    get_next_classes(classes)
